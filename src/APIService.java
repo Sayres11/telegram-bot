0fc5fac4c071a469fc0b93ddbaf75be71373d0da
Aliaksei <sayrespsn@gmail.com>
2022-03-27 21:54:14 +0200

    third commit

--- src/APIService.java
+++ src/APIService.java
@@ -6,20 +6,27 @@ import java.net.URL;
 public interface APIService {
 
     /**
-     *
      * @param api_request Specify the api method to execute
      */
-    default void request(String api_request) {
+    default String request(String api_request) {
         String urlString = "https://api.telegram.org/bot%s/" + api_request;
         urlString = String.format(urlString, Bot.getAPI_KEY());
+        StringBuffer textData = null;
 
         try {
             URL myURL = new URL(urlString);
             BufferedReader rd = new BufferedReader(new InputStreamReader(myURL.openConnection().getInputStream()));
-            System.out.println(rd);
+            textData = new StringBuffer();
+            String line;
+
+            while ((line = rd.readLine()) != null) {
+                textData.append(line);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return textData != null ? textData.toString() : "";
     }
 
     /**
@@ -34,9 +41,8 @@ public interface APIService {
         request(urlString);
     }
 
-/*    //FIXME
     default void getMessage(String chatId) {
         String urlString = "getUpdates?offset=%s";
         urlString = String.format(urlString, chatId);
-    }*/
+    }
 }
