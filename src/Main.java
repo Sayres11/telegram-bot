6b4b4baa8aed0260158759a0982bcc381d15f699
Aliaksei <44787029+Sayres11@users.noreply.github.com>
2023-02-27 12:24:36 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Bot bot = new Bot("5251768493:AAFkmg-oRe30T6UNv9h8Ntw3v19KRiaaOmM");
+        Bot bot = new Bot("API-KEY");
         bot.sendMessage("5251434810", "interface");
     }
 }
